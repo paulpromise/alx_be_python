6630c4a71b93076ab45f2a6d3427387f937333c4
Paul Promise <paul.promise@outlook.com>
2025-09-08 20:13:10 +0000

    Add basic_operations.py with examples of basic math operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+result = number1 + number2
+print(f"Addition of {number1} and {number2} is {result}")
+result = number1 - number2
+
+print(f"Subtraction of {number1} and {number2} is {result}")
+result = number1 * number2
+
+
+print(f"Multiplication of {number1} and {number2} is {result}")
