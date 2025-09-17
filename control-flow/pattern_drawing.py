c31dd431c665b49a7ceeb039aae156cf2b41e837
Paul Promise <paul.promise@outlook.com>
2025-09-17 23:09:34 +0000

    Add pattern_drawing.py: draws asterisk pattern of user-defined size

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,14 +1,11 @@
 # This program draws a size of the pattern using asterisks.
 
 while True:
-    try:
-        number_lines = int(input("Enter the size of the pattern: ").strip())
-        if number_lines > 0:
-            break
-        else:
-            print("Please enter a positive integer.")
-    except ValueError:
-        print("Invalid input. Please enter a positive integer.")
+    number_lines = int(input("Enter the size of the pattern: ").strip())
+    if number_lines > 0:
+        break
+    else:
+        print("Please enter a positive integer.")
 
 for _ in range(number_lines):
     print('*' * number_lines)
