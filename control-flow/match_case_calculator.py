9ba1de0205248a1b4e45ffec21be5b9cfda71939
Paul Promise <paul.promise@outlook.com>
2025-09-17 22:51:59 +0000

    modified the operator for the calculation

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -7,18 +7,18 @@ operation = input("Choose the operation (+, -, *, /): ").strip().lower()
 match operation:
     case "+":
         result = num1 + num2
-        print(f"The result of adding {num1} and {num2} is {result}.")
+        print(f"The result is {result}.")
     case "-":
         result = num1 - num2
-        print(f"The result of subtracting {num2} from {num1} is {result}.")
+        print(f"The result isis {result}.")
     case "*":
         result = num1 * num2
-        print(f"The result of multiplying {num1} and {num2} is {result}.")
+        print(f"The result is {result}.")
     case "/":
         if num2 != 0:
             result = num1 / num2
-            print(f"The result of dividing {num1} by {num2} is {result}.")
+            print(f"The result  is {result}.")
         else:
             print("Error: Division by zero is not allowed.")
     case _:
-        print("Invalid operation. Please choose add, subtract, multiply, or divide.")
+        print("Invalid operation. Please choose +, -, *, or /.")
