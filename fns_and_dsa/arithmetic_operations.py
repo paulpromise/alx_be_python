564c57b47d7f88a41102bcbed29cf4b57b93dd86
Paul Promise <paul.promise@outlook.com>
2025-09-23 11:48:21 +0000

    modified the functions for the right parameters

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,11 +7,11 @@ def perform_operation(num1, num2, operation):
         result = num1 - num2
     elif operation == 'multiply':
         result = num1 * num2
-    elif operation == 'divide':
-        if num2 != 0:
-            result = num1 / num2
-        else:
+    elif operation == 'divide': 
+        if num2 == 0:
             return "Division by zero error"
+        else:
+            result = num1 / num2
     else:
         return "Invalid operation"
 
