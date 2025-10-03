18b27d8d42a1ff513872d1893b1f182af1a5dc0c
Paul Promise <paul.promise@outlook.com>
2025-10-03 11:39:29 +0000

    fixed bug in the deposite method

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,14 +5,15 @@ class BankAccount:
 
     def deposit(self, amount):
         self.account_balance += amount
-        print(f"Deposited: ${amount}")
+        return amount
 
     def withdraw(self, amount):
-        if self.account_balance < 0:
-            print("Insufficient funds")
+        if self.account_balance < amount:
+            return False
         else:
             self.account_balance -= amount
             print(f"Withdrew: ${amount}")
+            return True
 
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance}")
