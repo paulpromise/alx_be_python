c3fda38a71d7e2869b4d05374a64ce9b1d1ff08f
Paul Promise <paul.promise@outlook.com>
2025-10-03 11:28:54 +0000

    fixed a bug in display_ballance method

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -15,4 +15,4 @@ class BankAccount:
             self.account_balance -= amount
 
     def display_balance(self):
-        print(f"Current balance: {self.account_balance}")
+        print(f"Current balance:{self.account_balance}")
